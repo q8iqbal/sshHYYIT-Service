46f659b15f4bccc92b8fe7cfb7bfb4884d97f45b
iqbal nur imansyah <iqbal.imansyah@btpn.com>
2022-11-27 23:53:56 +0700

    simsalabim selesai

--- src/ssh-log-parser.sh
+++ src/ssh-log-parser.sh
@@ -102,12 +102,12 @@ post_log(){
   echo "${BACKEND_URL}/log"
   curl --location --request POST "${BACKEND_URL}/log"\
   --data-raw '{
-      "ip_server": '${1}',
-      "hostname": '$(hostname)',
-      "ip_guest": '${2}',
-      "username": '${3}',
-      "timestamp": '${4}',
-      "status": '${5}'
+      "ip_server": "'${1}'",
+      "hostname": "'$(hostname)'",
+      "ip_guest": "'${2}'",
+      "username": "'${3}'",
+      "timestamp": "'${4}'",
+      "status": "'${5}'"
   }'
 
     # echo '{
