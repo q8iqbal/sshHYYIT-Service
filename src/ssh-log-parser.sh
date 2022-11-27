d0bc0a67b511dd4a8c30a7bfbe84f0bd568a9508
iqbal nur imansyah <iqbal.imansyah@btpn.com>
2022-11-28 00:28:58 +0700

    simsalabim selesai

--- src/ssh-log-parser.sh
+++ src/ssh-log-parser.sh
@@ -99,7 +99,7 @@ parse_log() {
 }
 
 post_log(){
-  curl -X POST -H 'Content-Type: application/json' -d' {"ip_server": "'$1'","hostname": "'$(hostname)'","ip_guest": "'$2'","username": "'$3'","timestamp": "'$4'","status": "'$5'"}' 'http://'${BACKEND_URL}'/log'
+  curl -X POST 'http://'${BACKEND_URL}'/log' -H 'Content-Type: application/json' -d' {"ip_server": "'$1'","hostname": "'$(hostname)'","ip_guest": "'$2'","username": "'$3'","timestamp": "'$4'","status": "'$5'"}'
 }
 
 parse_params "$@"
